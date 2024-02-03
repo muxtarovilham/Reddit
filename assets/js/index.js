980c80e47a3ca5ba88befaaa58a1f662325856ac
muxtarovilham <sd6y6dap3@code.edu.az>
2024-02-03 15:57:43 +0400

    Discovery and json server yesss

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,20 +1,52 @@
-const product = document.getElementById('posts-container')
+const productPopular = document.getElementById('popular-container')
+const productPost = document.getElementById('posts-container')
 
+// Popular Posts
 
 async function getProducts() {
-    const res = await axios.get('http://localhost:3000/posts')
-    const data = res.data
+    const res = await axios.get('http://localhost:5500/assets/json/db.json')
+    const data = res.data.posts
     db = data
     db.map(item => {
         const box = document.createElement('div')
         box.className = 'box col-12 col-sm-4 col-lg-3'
         box.innerHTML = `
-        <div class="boxes">
+        <a href="" class="boxes">
     <img src="${item.image}" alt="">
     <p>${item.description}</p>
+    <div class="users">
+    <img src="${item.userimage}" alt="">
+    <h1>${item.username}</h1>
 </div>
+</a>
         `
-        product.appendChild(box)
+        productPopular.appendChild(box)
     })
 }
 getProducts()
+
+
+// Posts 
+
+
+async function getPosts() {
+    const res = await axios.get('http://localhost:5500/assets/json/db.json')
+    const data = res.data.post
+    db = data
+    db.map(item => {
+        const box = document.createElement('div')
+        box.className = 'box col-12'
+        box.innerHTML = `
+        <div href="" class="boxes">
+        <div class="users">
+        <img src="${item.userimage}" alt="">
+        <h1>${item.username}</h1>
+        </div>
+        <p>${item.description}</p>
+    <img class="postImg" src="${item.image}" alt="">
+</div>
+        `
+        productPost.appendChild(box)
+    })
+}
+getPosts()
